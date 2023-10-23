4435718a2a95bb9b2fbd97c2deceecffaa404805
박규경 <64006112+ggpark1218@users.noreply.github.com>
2023-10-23 20:37:38 +0900

    [level unrated] Title: 문자열 밀기, Time: 0.05 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120921. 문자열 밀기/문자열 밀기.js
@@ -0,0 +1,14 @@
+function solution(A, B) {
+    let arrA = [...A];
+    
+    for(i=1; i<arrA.length; i++) {
+        if(A === B) {
+            return 0;
+        } else {
+            arrA.unshift(arrA.pop());
+            if(arrA.join('') === B) return i;
+        }
+    }
+    
+    return -1;
+}
